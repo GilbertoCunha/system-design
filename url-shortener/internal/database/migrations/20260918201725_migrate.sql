6e979fe8bb26d145960797e9a7bddf85b853d442
Gilberto Cunha <gilberto.cunha@grncunha.com>
2026-09-18 22:36:07 +0200

    feat: add postgresql migrations and fix docker compose starting order

--- url-shortener/internal/database/migrations/20260918201725_migrate.sql
+++ url-shortener/internal/database/migrations/20260918201725_migrate.sql
@@ -1,9 +1,13 @@
+-- +goose Up
 CREATE table IF NOT EXISTS urls (
   id BIGSERIAL PRIMARY KEY,
   short_url CHAR(32) UNIQUE NOT NULL,
   long_url TEXT UNIQUE NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
-
 CREATE UNIQUE INDEX IF NOT EXISTS idx_urls_short_url
 ON urls(short_url);
+
+-- +goose Down
+DROP INDEX IF EXISTS idx_urls_short_url;
+DROP TABLE IF EXISTS urls;
